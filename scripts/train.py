0cbc10af39076caca14e221d0e076fae01a8bbc0
alleboudy <ahmad.alleboudy@outlook.com>
2016-12-17 22:54:20 +0100

    defined separate loss functions for our pose and rotation outputs, trying training til tomorrow maybe
    Former-commit-id: 477765be85bdc950f2ff09b573e271f3b86bcdae

--- scripts/train.py
+++ scripts/train.py
@@ -2,25 +2,35 @@ import utilities
 import posenet
 import theano
 import numpy as np
+
+
+
 outputWeightspath = 'trainedweights.h5'
 BETA = 250 #to 2000 for outdoor
 directory = "/usr/prakt/w065/posenet/KingsCollege/"
 dataset = 'dataset_train.txt'
  
-def posenet_loss(y_true, y_pred):
-	print type(y_true)
-	print "####### IN THE LOSS FUNCTION #####"
-	xyz_true=y_true[0:3]
-	wpqr_true = y_true[3:7]
-	xyz_pred=y_pred[0:3]
-	wpqr_pred = y_pred[3:7]
-	return np.linalg.norm(xyz_true-xyz_pred) + BETA *np.linalg.norm(wpqr_true-wpqr_pred/np.linalg.norm(wpqr_pred))
 
-nb_epoch = 3
+
+def pose_loss(y_true, y_pred):
+	print "####### IN THE POSE LOSS FUNCTION #####"
+	return np.linalg.norm(y_true-y_pred) 
+
+def rotation_loss(y_true, y_pred):
+	print "####### IN THE ROTATION LOSS FUNCTION #####"
+	return  BETA *np.linalg.norm(y_true-y_pred/np.linalg.norm(y_pred))
+
+
+
+
+
+
+
+nb_epoch = 100
 print "creating the model"
 model =posenet.create_posenet('mergedweights.h5')
 #sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(optimizer='sgd', loss='mse')
+model.compile(optimizer='adam', loss=[pose_loss,rotation_loss])
 
 for e in range(nb_epoch):
 	print("epoch %d" % e)
