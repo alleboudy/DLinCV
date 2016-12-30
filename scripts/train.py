14b61f1b510812b8a70e492e169c38460ca322fa
alleboudy <ahmad.alleboudy@outlook.com>
2016-12-30 08:10:19 +0100

    added the other layers to the training, the error isn't stable, perhaps we need to shuffle the trining data or maybe the loss isn't working :S
    Former-commit-id: a5f1492c6b2c333ff9b47759b31bd3f20baccb25
    Former-commit-id: 9e8215b61265e8b1f7fa4179f53503f248e83ac8

--- scripts/train.py
+++ scripts/train.py
@@ -2,43 +2,53 @@ import utilities
 import posenet
 import theano
 import numpy as np
-
+from keras.optimizers import SGD
 
 
 outputWeightspath = 'trainedweights.h5'
-BETA = 250 #to 2000 for outdoor
+BETA = 500 #to 2000 for outdoor
 directory = "/usr/prakt/w065/posenet/KingsCollege/"
 dataset = 'dataset_train.txt'
  
 
 
-def pose_loss(y_true, y_pred):
+def pose_loss12(y_true, y_pred):
 	print "####### IN THE POSE LOSS FUNCTION #####"
-	return np.linalg.norm(y_true-y_pred) 
+	return 0.3* np.linalg.norm(y_true-y_pred) 
 
-def rotation_loss(y_true, y_pred):
+def rotation_loss12(y_true, y_pred):
 	print "####### IN THE ROTATION LOSS FUNCTION #####"
-	return  BETA *np.linalg.norm(y_true-y_pred/np.linalg.norm(y_pred))
+	return  150* np.linalg.norm(y_true-y_pred/np.linalg.norm(y_pred))
+
+
+def pose_loss3(y_true, y_pred):
+        print "####### IN THE POSE LOSS FUNCTION #####"
+        return  np.linalg.norm(y_true-y_pred)
+
+def rotation_loss3(y_true, y_pred):
+        print "####### IN THE ROTATION LOSS FUNCTION #####"
+        return  BETA *np.linalg.norm(y_true-y_pred/np.linalg.norm(y_pred))
 
 
 
 
 
-
-
-nb_epoch = 100
+nb_epoch = 30000
 print "creating the model"
 model =posenet.create_posenet('mergedweights.h5')
-#sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(optimizer='adam', loss=[pose_loss,rotation_loss])
+sgd = SGD(lr=0.00001, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(optimizer=sgd, loss=[pose_loss12,rotation_loss12,pose_loss12,rotation_loss12,pose_loss3,rotation_loss3])
 
 for e in range(nb_epoch):
 	print("epoch %d" % e)
 	for X_batch, Y_batch in utilities.BatchGenerator(32,directory,dataset):
 		#model.train(X_batch,Y_batch)
 		#history = model.fit(X_batch, Y_batch,batch_size=32,shuffle=True,nb_epoch=1)
-		history = model.fit(X_batch,{'cls3_fc_pose_wpqr': Y_batch[1], 'cls3_fc_pose_xyz': Y_batch[0]},
+		history = model.fit(X_batch,{'cls1_fc_pose_wpqr': Y_batch[1], 'cls1_fc_pose_xyz': Y_batch[0],'cls2_fc_pose_wpqr': Y_batch[1], 'cls2_fc_pose_xyz': Y_batch[0],'cls3_fc_pose_wpqr': Y_batch[1], 'cls3_fc_pose_xyz': Y_batch[0]},
           nb_epoch=1, batch_size=32)
 		print history.history['loss']
+		if e%500==0:
+			model.save_weights(outputWeightspath)
+
 
 model.save_weights(outputWeightspath)
