eba540ed6fd4684f381552915026f3dc00c02cf9
alleboudy <ahmad.alleboudy@outlook.com>
2017-02-06 23:42:19 +0100

    added oldhospital mean in DLinCV
    Former-commit-id: 1849b4a95382055803d2bd6eb50ce5ffce6d705e

--- converth5mean2npy.py
+++ converth5mean2npy.py
@@ -1,8 +1,9 @@
 import caffe
 import numpy as np
 import sys
-outfilename='namehere.py'
-binaryproto='/usr/prakt/w065/DLinCV/scripts/keras-posenet/smmean.binaryproto'
+outfilename='oldhospitalmean'
+
+binaryproto='scripts/lstm-keras-tf/oldhospitalmean.binaryproto'
 def getMean(meanFileLocation):
     blob = caffe.proto.caffe_pb2.BlobProto()
     data = open(meanFileLocation, 'rb').read()
