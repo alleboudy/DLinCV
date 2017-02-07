c304db8bced322507301cf965c00c117204655d4
Leboudy <ga48wed@mytum.de>
2017-02-07 02:09:16 +0100

    aa
    Former-commit-id: 3bd92d7754c95ff55efd177b5c071b2917e81585

--- scripts/lstm-keras-tf/seqtest.py
+++ scripts/lstm-keras-tf/seqtest.py
@@ -58,61 +58,63 @@ model.compile(optimizer=sgd, loss='categorical_crossentropy')
 #meantrasnformed =  np.expand_dims(meantrasnformed, axis=0)
 posxs = []
 posqs = []
-imgs=[]
-seq =[
-'seq13/frame00155.png 27.616028 -0.242176 54.205308 0.362095 -0.008852 0.924934 -0.115366',
-'seq13/frame00156.png 26.839205 -0.252827 54.305833 0.377098 -0.008740 0.916330 -0.134392',
-'seq13/frame00157.png 26.084117 -0.210158 54.388376 0.355471 -0.009817 0.925107 -0.133134'
+imgs = []
+seq = [
+    'seq13/frame00155.png 27.616028 -0.242176 54.205308 0.362095 -0.008852 0.924934 -0.115366',
+    'seq13/frame00156.png 26.839205 -0.252827 54.305833 0.377098 -0.008740 0.916330 -0.134392',
+    'seq13/frame00157.png 26.084117 -0.210158 54.388376 0.355471 -0.009817 0.925107 -0.133134'
 
 ]
+counter = 0
+with open(settings.testsetpath) as f:
+    inputs = np.zeros([1, 3, 3, 224, 224])
+    for line in f:
+        fname, p0, p1, p2, p3, p4, p5, p6 = line.split()
+        p0 = float(p0)
+        p1 = float(p1)
+        p2 = float(p2)
+        p3 = float(p3)
+        p4 = float(p4)
+        p5 = float(p5)
+        p6 = float(p6)
+        img = utilities.ResizeCropImage(
+            imread(directory + fname)).astype(np.float32)
 
+        img = img.transpose((2, 0, 1))
+        img[0, :, :] -= meanImage[0, :, :].mean()
+        img[1, :, :] -= meanImage[1, :, :].mean()
+        img[2, :, :] -= meanImage[2, :, :].mean()
+        inputs[0, counter % settings.stepSize, :, :, :] = img
+        counter += 1
+        if counter % 3 == 0:
+            out = model.predict(inputs)
+            posx = out[0]
+            posq = out[1]
+            print "actual:"
+            actualx = (p0, p1, p2)
+            actualq = (p3, p4, p5, p6)
+            q1 = actualq / np.linalg.norm(actualq)
+            q2 = posq / np.linalg.norm(posq)
+            d = abs(np.sum(np.multiply(q1, q2)))
+            theta = 2 * np.arccos(d) * 180 / np.pi
+            errx = np.linalg.norm(actualx - posx)
+            posxs.append(errx)
+            posqs.append(theta)
+            print 'errx ', errx, ' m and ', 'errq ', theta, ' degrees'
+            inputs = np.zeros([1, 3, 3, 224, 224])
+print 'median error', np.median(posxs), ' m and ', np.median(posqs), ' degrees'
 
-#meanout = model.predict(meantrasnformed)
-inputs = np.zeros([1,3,3,224,224])
-for i in range(settings.stepSize):
-    fname, p0, p1, p2, p3, p4, p5, p6 = seq[i].split()
-    p0 = float(p0)
-    p1 = float(p1)
-    p2 = float(p2)
-    p3 = float(p3)
-    p4 = float(p4)
-    p5 = float(p5)
-    p6 = float(p6)
     # poses.append((p0,p1,p2,p3,p4,p5,p6))
     # images.append(directory+fname)
-    img = utilities.ResizeCropImage(
-        imread(directory + fname)).astype(np.float32)
 
-    img = img.transpose((2, 0, 1))
-    img[0, :, :] -= meanImage[0, :, :].mean()
-    img[1, :, :] -= meanImage[1, :, :].mean()
-    img[2, :, :] -= meanImage[2, :, :].mean()
-    #img[:,:,[0,1,2]] = img[:,:,[2,1,0]]
-    #img = np.expand_dims(img, axis=0)
-    inputs[0,i,:,:,:] = img
-    #imgs.append(img)
-#print len(imgs)
-#print np.asarray(imgs).shape
-out = model.predict(inputs)  # note: the model has three outputs
-#out = model.predict(np.expand_dims(np.asarray(imgs),axis=0))  # note: the model has three outputs
+    # imgs.append(img)
+# print len(imgs)
+# print np.asarray(imgs).shape
+  # note: the model has three outputs
+# out = model.predict(np.expand_dims(np.asarray(imgs),axis=0))  # note: the model has three outputs
 # for i in range(len(out)):
 #	for j in range(len(out[i])):
 #		out[i][j]+=meanout[i][j]
 # print np.argmax(out[2])
-print out
-#print "predcited:"
-posx = out[0]
-posq = out[1]
-print "actual:"
-actualx = (p0, p1, p2)
-actualq = (p3, p4, p5, p6)
-q1 = actualq / np.linalg.norm(actualq)
-q2 = posq / np.linalg.norm(posq)
-d = abs(np.sum(np.multiply(q1, q2)))
-theta = 2 * np.arccos(d) * 180 / np.pi
-errx = np.linalg.norm(actualx - posx)
-posxs.append(errx)
-posqs.append(theta)
-print 'errx ', errx, ' m and ', 'errq ', theta, ' degrees'
-print 'median error', np.median(posxs), ' m and ', np.median(posqs), ' degrees'
-
+# print out
+# print "predcited:"
