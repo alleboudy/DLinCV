3001870eba6fbb8a08005e5d4edec5627ab53e51
alleboudy <ahmad.alleboudy@outlook.com>
2017-02-09 21:57:56 +0100

    -
    Former-commit-id: 59a0018018665d0119b2f0fec9fc470beeece2a9

--- scripts/lstm-keras-tf/train.py
+++ scripts/lstm-keras-tf/train.py
@@ -60,6 +60,7 @@ for i in range(nb_epochs):
 		#model.train(X_batch,Y_batch)
 		#history = model.fit(X_batch, Y_batch,batch_size=32,shuffle=True,nb_epoch=1)
 	print Y_batch[0].shape
+	print Y_batch[1].shape
 	print len(Y_batch)	
 	history = model.fit(X_batch,{'pose_wpqr': Y_batch[1], 'pose_xyz': Y_batch[0]},
           nb_epoch=1,batch_size=utilities.batchSize)
