534115a64a5a4553751eb4e3d5ccfacc6ab1f58c
alleboudy <ahmad.alleboudy@outlook.com>
2017-02-10 15:19:04 +0100

    adding a siilarity measure
    Former-commit-id: 3b27cb0c537b0040a63f92a24b2001e5feae662a

--- scripts/lstm-keras-tf/seqtest.py
+++ scripts/lstm-keras-tf/seqtest.py
@@ -59,16 +59,12 @@ model.compile(optimizer=sgd, loss='categorical_crossentropy')
 posxs = []
 posqs = []
 imgs = []
-seq = [
-    'seq13/frame00155.png 27.616028 -0.242176 54.205308 0.362095 -0.008852 0.924934 -0.115366',
-    'seq13/frame00156.png 26.839205 -0.252827 54.305833 0.377098 -0.008740 0.916330 -0.134392',
-    'seq13/frame00157.png 26.084117 -0.210158 54.388376 0.355471 -0.009817 0.925107 -0.133134'
-
-]
+howmanyaccepted=0
+similarityThreshold=settings.similarityThreshold
 counter = 0
 with open(settings.testsetpath) as f:
     inputs = np.zeros([1, 3, 3, 224, 224])
-    labels = np.zeros([1,3, 7])
+    labels = np.zeros([3, 7])
     for line in f:
         fname, p0, p1, p2, p3, p4, p5, p6 = line.split()
         p0 = float(p0)
@@ -78,25 +74,28 @@ with open(settings.testsetpath) as f:
         p4 = float(p4)
         p5 = float(p5)
         p6 = float(p6)
-        img = utilities.ResizeCropImage(
+        posexyzwpqr = np.asarray((p0,p1,p2,p3,p4,p5,p6))
+	img = utilities.ResizeCropImage(
             imread(directory + fname)).astype(np.float32)
-
+	
         img = img.transpose((2, 0, 1))
         img[0, :, :] -= meanImage[0, :, :].mean()
         img[1, :, :] -= meanImage[1, :, :].mean()
         img[2, :, :] -= meanImage[2, :, :].mean()
         inputs[0, counter % settings.stepSize, :, :, :] = img
-	labels[:,counter % settings.stepSize, :] = np.asarray((p0,p1,p2,p3,p4,p5,p6))
+	labels[counter % settings.stepSize, :] =posexyzwpqr #np.asarray((p0,p1,p2,p3,p4,p5,p6))
+	
         counter += 1
         if counter % 3 == 0:
             out = model.predict(inputs)
 	   # print out
-	    print out[0].shape
-            posx = out[0][0][0]
-            posq = out[1][0][0]
+	   # print out[0].shape #(1,3,3)
+	    #print out
+            posx = out[0][0].mean(0)#xyz
+            posq = out[1][0].mean(0)#wpqr
             print "actual:"
-            actualx = labels[:,0,:3]
-            actualq = labels[:,0,3:7]
+            actualx = labels[:,:3].mean(0)
+            actualq = labels[:,3:7].mean(0)
             q1 = actualq / np.linalg.norm(actualq)
             q2 = posq / np.linalg.norm(posq)
             d = abs(np.sum(np.multiply(q1, q2)))
@@ -106,7 +105,7 @@ with open(settings.testsetpath) as f:
             posqs.append(theta)
             print 'errx ', errx, ' m and ', 'errq ', theta, ' degrees'
             inputs = np.zeros([1, 3, 3, 224, 224])
-	    labels = np.zeros([1, 3, 7])
+	    labels = np.zeros([ 3, 7])
 print 'median error', np.median(posxs), ' m and ', np.median(posqs), ' degrees'
 
     # poses.append((p0,p1,p2,p3,p4,p5,p6))
