497abec7f0d8521d8b882314650401a27084742c
Leboudy <ga48wed@mytum.de>
2017-02-18 14:05:52 +0100

    ةة
    Former-commit-id: 10d60dbe50c04c7cdb2c51badedcc2fd6b07eefd

--- scripts/lstm-keras-tf/scripts/train.py
+++ scripts/lstm-keras-tf/scripts/train.py
@@ -59,9 +59,9 @@ for i in range(nb_epochs):
 	X_batch, Y_batch = next(data_gen)
 		#model.train(X_batch,Y_batch)
 		#history = model.fit(X_batch, Y_batch,batch_size=32,shuffle=True,nb_epoch=1)
-	print Y_batch[0].shape
-	print Y_batch[1].shape
-	print len(Y_batch)	
+	#print Y_batch[0].shape
+	#print Y_batch[1].shape
+	#print len(Y_batch)	
 	history = model.fit(X_batch,Y_batch,
           nb_epoch=1,batch_size=utilities.batchSize)
 	#history = model.fit(X_batch,{'pose_wpqr': Y_batch[1], 'pose_xyz': Y_batch[0]},
