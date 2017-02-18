9bdbb8d444c196c33c7fc37e1efa8fe3e0887e7a
Leboudy <ga48wed@mytum.de>
2017-02-18 10:47:46 +0100

    fixed tester
    Former-commit-id: 41b7c94e6662cf6c8ddeb27c5e3ba770a4e72023

--- scripts/lstm-keras-tf/seqtest.py
+++ scripts/lstm-keras-tf/seqtest.py
@@ -13,15 +13,15 @@ from LRN2D import LRN2D as LRN
 import settings
 from similarityMeasures import getError
 directory = settings.directory  # "/usr/prakt/w065/posenet/sm/"
-#datasource = utilities.get_data(settings.testsetpath)
-#datagen = utilities.gen_data(datasource)
+datasource = utilities.get_data(settings.testsetpath)
+datagen = utilities.gen_data(datasource)
 #outputDirectory = "/usr/prakt/w065/posenet/TFData/"
 #meanFileLocation = 'smmean.binaryproto'
 # 'tfsmtrainedweights.h5'#'75batbhessmtrainedweights.h5'#'smtrainedweights.h5'
 weightsfile = settings.outputWeightspath
 # weightsfile='shoptrainedweights.h5'
-poses = []  # will contain poses followed by qs
-images = []
+#poses = []  # will contain poses followed by qs
+#images = []
 
 # limitingCounter=3
 # def getMean():
@@ -48,7 +48,7 @@ images = []
 #cv2.imshow("resized", resized)
 # cv2.waitKey(0)
 
-meanImage = utilities.getMean()
+#meanImage = utilities.getMean()
 # print meanImage.shape
 # Test pretrained model
 model = cnn_lstm.create_cnn_lstm(weightsfile)
@@ -59,63 +59,35 @@ model.compile(optimizer=sgd, loss='categorical_crossentropy')
 #meantrasnformed =  np.expand_dims(meantrasnformed, axis=0)
 posxs = []
 posqs = []
-imgs = []
+#imgs = []
 howmanyaccepted=0
 
 counter = 0
 
 
-with open(settings.testsetpath) as f:
+#with open(settings.testsetpath) as f:
+    #inputs = np.zeros([1, 3, 3, 224, 224])
+    #labels = np.zeros([3, 7])
+for ims,xs,qs in datagen:
+    howmanyaccepted+=1
+    print howmanyaccepted
     inputs = np.zeros([1, 3, 3, 224, 224])
-    labels = np.zeros([3, 7])
-    for line in f:
-        fname, p0, p1, p2, p3, p4, p5, p6 = line.split()
-        p0 = float(p0)
-        p1 = float(p1)
-        p2 = float(p2)
-        p3 = float(p3)
-        p4 = float(p4)
-        p5 = float(p5)
-        p6 = float(p6)
-        posexyzwpqr = np.asarray((p0,p1,p2,p3,p4,p5,p6))
-	img = utilities.ResizeCropImage(
-            imread(directory + fname)).astype(np.float32)
-	
-        img = img.transpose((2, 0, 1))
-        img[0, :, :] -= meanImage[0, :, :].mean()
-        img[1, :, :] -= meanImage[1, :, :].mean()
-        img[2, :, :] -= meanImage[2, :, :].mean()
-        inputs[0, counter % settings.stepSize, :, :, :] = img
-	labels[counter % settings.stepSize, :] =posexyzwpqr #np.asarray((p0,p1,p2,p3,p4,p5,p6))
-	
-        counter += 1
-        if counter % 3 == 0:
-            m1_2,a1_2 = getError(labels[0][:3],labels[0][3:7],labels[1][:3],labels[1][3:7])
-            m2_3,a2_3 = getError(labels[1][:3],labels[1][3:7],labels[2][:3],labels[2][3:7])
-            m1_3,a1_3 = getError(labels[0][:3],labels[0][3:7],labels[2][:3],labels[2][3:7])
-            
-            if m1_2 >settings.distanceThreshold or m2_3 >settings.distanceThreshold: 
-		print m1_2,m2_3
-                continue
-            if a1_2 >settings.angleThreshold or a2_3 >settings.angleThreshold: 
-                print a1_2,a2_3
-		continue
-            howmanyaccepted+=1
-            out = model.predict(inputs)
+    inputs[0,:]=ims
+    out = model.predict(inputs)
 	   # print out
 	   # print out[0].shape #(1,3,3)
 	    #print out
-            posx = out[0][0][1]#.mean(0)#xyz
-            posq = out[1][0][1]#.mean(0)#wpqr
+    posx = out[0][0][1]#.mean(0)#xyz
+    posq = out[1][0][1]#.mean(0)#wpqr
             #print "actual:"
-            actualx = labels[:,:3][1]#.mean(0)
-            actualq = labels[:,3:7][1]#.mean(0)
-            errx, theta = getError(posx,posq,actualx,actualq)
-            posxs.append(errx)
-            posqs.append(theta)
-            print 'errx ', errx, ' m and ', 'errq ', theta, ' degrees'
-            inputs = np.zeros([1, 3, 3, 224, 224])
-	    labels = np.zeros([ 3, 7])
+    actualx = xs[1]#.mean(0)
+    actualq = qs[1]#.mean(0)
+    errx, theta = getError(posx,posq,actualx,actualq)
+    posxs.append(errx)
+    posqs.append(theta)
+    print 'errx ', errx, ' m and ', 'errq ', theta, ' degrees'
+    #inputs = np.zeros([1, 3, 3, 224, 224])
+	#labels = np.zeros([ 3, 7])
 print 'median error', np.median(posxs), ' m and ', np.median(posqs), ' degrees'
 print 'accepted test sequences: ',howmanyaccepted
 
